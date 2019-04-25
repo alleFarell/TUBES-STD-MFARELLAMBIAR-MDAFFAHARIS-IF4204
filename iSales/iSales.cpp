1a107f349c6161f27f340bdc022971ec4e788568
Mohammaddaffaharis <46914307+Mohammaddaffaharis@users.noreply.github.com>
2019-04-25 22:03:40 +0700

    NULL

--- iSales/iSales.cpp
+++ iSales/iSales.cpp
@@ -1,15 +1,101 @@
-#include "iSales.h"
+#include "kategori.h"
 
 void createList(List &L){
     First(L) = NULL;
     Last(L) = NULL;
 }
 
-adrKategori allocationKat(string kategori){
+void createList(ListBrg &L2){
+    First(L2) = NULL;
+    Last(L2) = NULL;
+}
+
+bool isEmpty(List L){
+    if(First(L)==NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+adrKategori allocationKat(infotypeK kategori){
     adrKategori P = new elmlistKategori;
     infoK(P) = kategori;
     nextK(P) = NULL;
     prevK(P) = NULL;
-    FirstBrg(P) = NULL;
-    LastBrg(P) = NULL;
+    ListBrg L2;
+    createList(L2);
+}
+void insertFirst(List &L, adrKategori P){
+    if (First(L) != NULL){
+        nextK(P)= First(L);
+        prevK(First(L))= P;
+        First(L)=P;
+    } else{
+        First(L) = P;
+        Last(L) = P;
+    }
+}
+
+void insertLast(List &L, adrKategori P){
+    if(P!= NULL){
+        prevK(P) = Last(L);
+        nextK(Last(L)) = P;
+        Last(L) = P;
+    } else{
+        insertFirst(L,P);
+    }
+}
+
+void deleteFirst(List &L, adrKategori &P){
+    P = First(L);
+    if(First(L)!= Last(L)){
+        First(L) = nextK(P);
+        prevK(First(L)) = NULL;
+        nextK(P) = NULL;
+    } else{
+        First(L) = NULL;
+        Last(L) = NULL;
+    }
+
+}
+
+void deleteLast(List &L, adrKategori &P){
+    if(First(L)!=Last(L)){
+        P = Last(L);
+        Last(L) = prevK(Last(L));
+        nextK(Last(L)) = NULL;
+        prevK(P) = NULL;
+    } else{
+        deleteFirst(L,P);
+    }
+}
+
+void printParent(List L){
+    adrKategori P = First(L);
+    while (P!=NULL){
+        cout<<infoK(P)<<" - ";
+        P = nextK(P);
+    }
+};
+
+adrKategori searchParent(List L, infotypeK kategori){
+    adrKategori P = First(L);
+    do{
+        if(infoK(P)== kategori){
+            return P;
+        } else{
+            P= nextK(P);
+        }
+
+    } while(P != Last(L));
+    return NULL;
+}
+
+void insertBarang(List &L, infotypeK kategori, infotypeB barang){
+    adrKategori P = searchParent(L,kategori);
+    if (P!=NULL){
+
+    }
 }
--- iSales/main.cpp
+++ iSales/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#include "kategori.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    List L;
+    createList(L);
+    adrKategori P = allocationKat("Makanan");
+    insertFirst(L,P);
+    P = allocationKat("Minuman");
+    insertFirst(L,P);
+    P = allocationKat("ATK");
+    insertFirst(L,P);
+    P = allocationKat("alat Kungfu");
+    insertFirst(L,P);
+    deleteLast(L,P);
+
+    P = searchParent(L,"kuntul");
+    if(P==NULL){
+        cout<<"Not Found"<<endl;
+    }
+    else{
+        cout<<"Ditemukan: "<<infoK(P)<<endl;
+    }
+
+
+
+
+
+    cout<<"Parent: "; printParent(L);
+
 }
